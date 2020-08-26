498c9f373ecd1f30f03acb2155c8b4812d1aa48b
Xerox <alfredo.montoya@cesunbc.edu.mx>
2020-08-26 15:46:14 -0700

    Sass update.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Header from './components/Header';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
+import './assets/styles/main.scss';
+
 function App() {
   return (
     <div className="App">
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import '../assets/styles/components/Header.scss';
-
 const Header = () =>{
     return(
         <header>
