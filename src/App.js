3cc2d5d441e48b7168833694ddb11cf317bf8b30
María Andrea <pericoandrea@gmail.com>
2020-08-27 01:06:41 -0500

    api and phot style

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Normalize from 'react-normalize';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
 import Main from './components/Main';
 import Footer from './components/Footer';
+import api from './api';
 
 import './fontawesome';
 /* flaticon */
 import './assets/font/flaticon.css';
 import './assets/sass/main.scss';
 
-function App() {
-  return (
-    <div className='App'>
-      <Normalize />
-      <Navbar />
-      <Header />
-      <Main />
-      <Footer />
-    </div>
-  );
-}
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      error: null,
+      data: undefined,
+      modalIsOpen: false,
+    };
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData = async () => {
+
+    this.setState({ loading: true, error: null });
+
+    try {
+      const data = await api.members.list();
+      this.setState({ data });
+    } catch (error) {
+      this.setState({ loading: false, error });
+    }
+  };
+
+  render() {
+    const { data } = this.state;
+    if (data) {
+      return (
+        <>
+          <Normalize />
+          <Navbar members={data} />
+          <Header />
+          <Main />
+          <Footer />
+        </>
+      );
+    }
+    return <h1>Loading</h1>;
+
+  }
+};
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,43 +2,18 @@ import React from 'react';
 
 import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import Avatar1 from '../assets/images/img-perfil-1.jpeg';
-import Avatar2 from '../assets/images/img-perfil-2.jpeg';
-import Avatar3 from '../assets/images/img-perfil-3.jpg';
 
-const Navbar = () => {
+function Navbar(props) {
   return (
     <nav className='slide'>
       <div className='slide__group'>
 
-        <a className='slide__item fourth-position' href='/'>
-          <img src={Avatar1} alt='' />
-          Jobana Garavito
-        </a>
-        <a className='slide__item third-position' href='/'>
-          <img src={Avatar2} alt='' />
-          Omar Montoya
-        </a>
-        <a className='slide__item second-position' href='/'>
-          <img src={Avatar3} alt='' />
-          María Andrea Perico
-        </a>
-        <a className='slide__item first-position' href='/'>
-          <img src={Avatar2} alt='' />
-          Erik Manuel Guevara
-        </a>
-        <a className='slide__item second-position' href='/'>
-          <img src={Avatar1} alt='' />
-          Paola Porras
-        </a>
-        <a className='slide__item third-position' href='/'>
-          <img src={Avatar2} alt='' />
-          Sergio Ruiz
-        </a>
-        <a className='slide__item fourth-position' href='/'>
-          <img src={Avatar1} alt='' />
-          Jaime Escobedo
-        </a>
+        {props.members.map((member, index) => (
+          <a key={`index-${member.fullname}`} className='slide__item' href='/'>
+            <img src={member.photo} alt='' />
+            <span>{member.fullname}</span>
+          </a>
+        ))}
 
         {/* <a className="slide__item fourth-position" href="/"><img src={avatar1} alt=""/>Jobana Garavito</a>
                 <a className="slide__item third-position" href="/"><img src={avatar2} alt=""/>Omar Montoya</a>
@@ -49,8 +24,8 @@ const Navbar = () => {
                 <a className="slide__item fourth-position" href="/"><img src={avatar1} alt=""/>Jaime Escobedo</a> */}
       </div>
       <div className='slide__buttons'>
-        <button className='slide__buttons-single'><span><FontAwesomeIcon icon={faAngleLeft} /></span></button>
-        <button className='slide__buttons-single'><span><FontAwesomeIcon icon={faAngleRight} /></span></button>
+        <button type='button' className='slide__buttons-single'><span><FontAwesomeIcon icon={faAngleLeft} /></span></button>
+        <button type='button' className='slide__buttons-single'><span><FontAwesomeIcon icon={faAngleRight} /></span></button>
       </div>
     </nav>
   );
